3ae3d329efd6841353f46eee737b4a703059e3c1
fares <fereshouidi298@gmail.com>
2025-08-22 19:16:17 +0100

    some edit

--- server.ts
+++ server.ts
@@ -37,9 +37,8 @@ const io = new Server(server, {
 });
 
 app.get("/", (req, res) => {
-  res.send("Server is working!");
+  res.send(`Server is working! at port: ${process.env.PORT || 3003}`);
   console.log(`Server is working! at port: ${process.env.PORT || 3003}`);
-
 });
 
 app.get("/getAllClients", async (req, res) => {
